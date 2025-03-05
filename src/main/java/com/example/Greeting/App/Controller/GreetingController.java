c2b6d85a8a33688bcaeea296edf08b949354e436
abhinavgupta1309 <guptaabhinav1309@gmail.com>
2025-03-05 11:33:24 +0530

    Greeting app with different messages

--- src/main/java/com/example/Greeting/App/Controller/GreetingController.java
+++ src/main/java/com/example/Greeting/App/Controller/GreetingController.java
@@ -22,4 +22,10 @@ public class GreetingController {
         return GreetingService.getGreetingMessage();
     }
 
+    @GetMapping("/personalized")
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName, lastName);
+    }
+
 }
