657872e846c33a6dce13a098fd5c8a5c559d85d8
abhinavgupta1309 <guptaabhinav1309@gmail.com>
2025-03-06 14:31:20 +0530

    Implemented User Registration and login functionality

--- src/main/java/com/example/Greeting/App/Controller/GreetingController.java
+++ src/main/java/com/example/Greeting/App/Controller/GreetingController.java
@@ -1,3 +1,4 @@
+
 package com.example.Greeting.App.Controller;
 import com.example.Greeting.App.Model.Greeting;
 import com.example.Greeting.App.Service.GreetingService;
@@ -13,16 +14,14 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+    @Autowired
+    GreetingService greetingService;
+
     @GetMapping
     public String getGreeting() {
         return "Hello, this is a GET request!";
     }
 
-    private final GreetingService greetingService;
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
 
     @GetMapping("/service")
     public String getGreetingServiceLayer() {
